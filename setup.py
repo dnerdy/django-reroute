383c0ab9c355ae2bea02bb918cedbb8e5b813115
Mark Sandstrom <mark@deliciouslynerdy.com>
2010-12-10 12:32:54 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+# Remember to change in reroute/__init__.py as well!
+VERSION = '0.1.0'
+
+setup(
+    name='django-reroute',
+    version=VERSION,
+    description="A drop-in replacement for django.conf.urls.defaults which supports HTTP verb dispatch and view wrapping.",
+    author='Mark Sandstrom',
+    author_email='mark@deliciouslynerdy.com',
+    url='http://github.com/dnerdy/reroute',
+    keywords=['reroute', 'django', 'http', 'rest', 'route', 'routing', 'dispatch', 'wrapper'],
+    packages=['reroute'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
